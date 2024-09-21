a6e50f368432d5931452a608e84a479a8f471b13
artemmm5 <artemsemenov06112012@yandex.ru>
2024-09-21 18:05:06 +0700

    Add files via upload

---
+++ 2lesson, work1.py
@@ -0,0 +1,9 @@
+name = input("Как дела?")
+
+
+print(1)
+print()
+print(11)
+print()
+print(111)
+print("Я вам не мешаю?")
---
+++ 2lesson, work2.py
@@ -0,0 +1,6 @@
+name = input("Как тебя зовут?")
+otchestvo = input("А отчество?")
+
+print("Здравствуйте",  name , otchestvo )
+
+
